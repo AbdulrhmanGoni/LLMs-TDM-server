9a15a4e19deba87553c1e7937a06f986af96d4c9
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-19 18:14:19 +0300

    feat: create registering routes utilities in the custom router
    Add `registerRoute` method to `Router` and make it uses
    `src/router/registerRoute.ts` function to do registering routes
    task,
    
    Add five methods (`GET`, `POST`, `DELETE`, `PATCH` and `PUT`)
    to `Router` class for providing a simple api to regiter route
    need to be handled easily.

--- src/router/Router.ts
+++ src/router/Router.ts
@@ -1,4 +1,5 @@
 import type { RequestHandler } from "../types/request";
+import registerRoute from "./registerRoute";
 
 type RouteData = {
   handlers: RequestHandler[];
@@ -16,6 +17,30 @@ const RouterClass = (function () {
 
   class Router {
     constructor() {}
+
+    registerRoute(method: string, path: string, ...handlers: RequestHandler[]) {
+      registerRoute(routes, method, path, ...handlers);
+    }
+
+    GET(path: string, ...handlers: RequestHandler[]) {
+      this.registerRoute(this.GET.name, path, ...handlers);
+    }
+
+    POST(path: string, ...handlers: RequestHandler[]) {
+      this.registerRoute(this.POST.name, path, ...handlers);
+    }
+
+    DELETE(path: string, ...handlers: RequestHandler[]) {
+      this.registerRoute(this.DELETE.name, path, ...handlers);
+    }
+
+    PUT(path: string, ...handlers: RequestHandler[]) {
+      this.registerRoute(this.PUT.name, path, ...handlers);
+    }
+
+    PATCH(path: string, ...handlers: RequestHandler[]) {
+      this.registerRoute(this.PATCH.name, path, ...handlers);
+    }
   }
 
   return Router;
