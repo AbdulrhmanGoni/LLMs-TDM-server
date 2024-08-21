17431933219f7364dad9669eda3c88cccf00ca7b
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-21 11:46:18 +0300

    feat: create `SuccessResponse` function
    Create `SuccessResponse` function in `src/utilities` directory
    which is a utility function to provide a shorthand of
    successfull responses.

---
+++ src/utilities/SuccessResponse.ts
@@ -0,0 +1,8 @@
+export default function JsonResponse(
+  data?: Record<string, any> | string | boolean,
+  message?: string,
+  statusCode: number = 200,
+  statusText: string = "success"
+): Response {
+  return Response.json({ data, message }, { status: statusCode, statusText });
+}
