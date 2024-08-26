a72897d000e66bc6ad517c20dd28d56b3fcb31ce
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-26 15:22:05 +0300

    feat: create `Validator` class
    Create `Validator` class which represents a single value
    (string or number) in requests inputs validation process,
    This class has two properties and two methods:
    
    - `rules` property, which is an array of validations functions
    take the value as parameter and return either true (which
    means the value is valid) or a string describes the reason
    why the value isn't valid.
    
    - `isOptional` property, which is a boolean value specifies
    whether the value is optional or not.
    
    - `addRule` method for adding validation rules to the value
    and for using these rules later to validate the value.
    
    - `validate` method which is used to validate the value
    using the rules in `rules` array property and `isOptional`
    property.

---
+++ src/validation/Validator.ts
@@ -0,0 +1,30 @@
+export type ValidationRule<T> = (value: T) => boolean | string;
+
+export default class Validator<T> {
+  private rules: ValidationRule<T>[] = [];
+  public isOptional: boolean = true;
+
+  addRule(rule: ValidationRule<T>): this {
+    this.rules.push(rule);
+    return this;
+  }
+
+  validate(value: T, field?: string): string | true {
+    if (value === undefined) {
+      if (this.isOptional) {
+        return true;
+      } else {
+        return `${field} field is required !`;
+      }
+    }
+
+    for (const rule of this.rules) {
+      const result = rule(value);
+      if (typeof result === "string") {
+        return result;
+      }
+    }
+
+    return true;
+  }
+}
