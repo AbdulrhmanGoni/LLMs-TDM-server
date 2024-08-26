8cab54fe41b1ae7286efc2e96c975a87108e5165
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-26 17:16:07 +0300

    feat: create `StringValidator` class
    Create `StringValidator` class for strings validation
    
    The class uses an instance of `Validator` class to represent
    the strings and validate them, it provides five string validation
    rules as methods which are: `required`, `max`, `min`, `matches`
    and `objectId`, these methods are used to add validation rules
    to `validator` instance for using them later by `validate` method
    to validate any value.
    
    Usage:
    it can be used by caling `stringValidator` function to get
    an instance of `StringValidator` class and then calling
    the validation methods to add validation rules (the instance
    of `StringValidator` is chainable, so it is possible to chaine
    multi rules in one line),  And can be validated using added
    rules by calling `validate` method.

---
+++ src/validation/stringValidator.ts
@@ -0,0 +1,52 @@
+import {
+  isObjectId,
+  isRequired,
+  isString,
+  matches,
+  maxLength,
+  minLength,
+} from "./validationUtils";
+import Validator from "./Validator";
+
+export class StringValidator {
+  validator: Validator<string>;
+
+  constructor() {
+    this.validator = new Validator<string>();
+    this.validator.addRule(isString());
+  }
+
+  validate(value: any, field?: string) {
+    return this.validator.validate(value, field);
+  }
+
+  required(message?: string): this {
+    this.validator.addRule(isRequired(message));
+    this.validator.isOptional = false;
+    return this;
+  }
+
+  max(min: number, message?: string): this {
+    this.validator.addRule(maxLength(min, message));
+    return this;
+  }
+
+  min(min: number, message?: string): this {
+    this.validator.addRule(minLength(min, message));
+    return this;
+  }
+
+  matches(regExp: RegExp, message?: string): this {
+    this.validator.addRule(matches(regExp, message));
+    return this;
+  }
+
+  objectId(message?: string): this {
+    this.validator.addRule(isObjectId(message));
+    return this;
+  }
+}
+
+export default function stringValidator() {
+  return new StringValidator();
+}
