219e735347c44c699bfb6b9ae411f726e29496ec
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-27 11:07:39 +0300

    feat: create `createDatasetInputValidator` middleware
    Create `src/middlewares` directory and inside this dir
    create the file of `createDatasetInputValidator` middleware
    
    This middleware is used to validate the inputs of the
    incomming requests to create new datasets using
    `validationSchema` function, if the inputs are valid,
    the middleware will let the next request handler (mostly,
    a controller or another middleware) takes the request
    and process it, if they are not valid, the middleware
    will return the validation error that will be thrown from
    `validate` method of `validationSchema` function's
    returned object as response with status code 403.

---
+++ src/middlewares/createDatasetInputValidator.ts
@@ -0,0 +1,20 @@
+import ErrorResponse from "../utilities/ErrorResponse";
+import type { DatasetInput } from "../types/datasets";
+import type { Req } from "../types/request";
+import type { ValidationError } from "../types/validation";
+import DatasetSchemaRule from "../validation/DatasetSchemaRule";
+import validationSchema from "../validation/validationSchema";
+
+const datasetInput = validationSchema<DatasetInput>({
+  name: DatasetSchemaRule().name.required(),
+  description: DatasetSchemaRule().description.required(),
+});
+
+export default function createDatasetInputValidator(request: Req) {
+  try {
+    request.json = datasetInput.validate(request.json);
+  } catch (e: any) {
+    const validationErrors = e.errors as ValidationError;
+    return ErrorResponse({ validationErrors }, 403);
+  }
+}
