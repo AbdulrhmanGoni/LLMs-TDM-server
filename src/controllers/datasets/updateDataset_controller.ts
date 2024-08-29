af90c0d8b5d1d2252d314548be7c181d2396db16
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-29 11:27:30 +0300

    feat: create `updateDataset_controller` controller
    Create `updateDataset_controller` controller which is
    the request handler function that receives the
    requests of updating datasets and calls the service
    `datasetsService` to perform the updating operations
    and returns respones based on the result of service's
    operation.

---
+++ src/controllers/datasets/updateDataset_controller.ts
@@ -0,0 +1,24 @@
+import ErrorResponse from "../../utilities/ErrorResponse";
+import InternalServerErrorResponse from "../../utilities/InternalServerErrorResponse";
+import SuccessResponse from "../../utilities/SuccessResponse";
+import datasetsService from "../../services/datasets";
+import type { Req } from "../../types/request";
+
+export default async function updateDataset_controller(
+  request: Req
+): Promise<Response> {
+  try {
+    const { message, isSuccess, result } = await datasetsService.updateDataset(
+      request.params.datasetId,
+      request.json
+    );
+
+    if (isSuccess) {
+      return SuccessResponse(result, message);
+    } else {
+      return ErrorResponse(message || "Dataset creation failed", 400);
+    }
+  } catch {
+    return InternalServerErrorResponse();
+  }
+}
