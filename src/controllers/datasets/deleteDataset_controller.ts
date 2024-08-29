bfdaf5f4efe4354e6aaf765022e37dae7e313c84
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-29 11:26:38 +0300

    feat: create `deleteDataset_controller` controller
    Create `deleteDataset_controller` controller which is
    the request handler function that receives the
    requests of datasets deletion and calls the service
    `datasetsService` to perform the deletion operations
    and returns respones based on the result of service's
    operation.

---
+++ src/controllers/datasets/deleteDataset_controller.ts
@@ -0,0 +1,23 @@
+import ErrorResponse from "../../utilities/ErrorResponse";
+import InternalServerErrorResponse from "../../utilities/InternalServerErrorResponse";
+import SuccessResponse from "../../utilities/SuccessResponse";
+import datasetsService from "../../services/datasets";
+import type { Req } from "../../types/request";
+
+export default async function deleteDataset_controller(
+  request: Req
+): Promise<Response> {
+  try {
+    const { message, isSuccess, result } = await datasetsService.deleteDataset(
+      request.params.datasetId
+    );
+
+    if (isSuccess) {
+      return SuccessResponse(result, message);
+    } else {
+      return ErrorResponse(message || "Dataset deletion failed", 400);
+    }
+  } catch {
+    return InternalServerErrorResponse();
+  }
+}
