3465ca6ef912bac223c5c18eaa0231121bc54409
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-31 17:44:01 +0300

    feat: create `deleteInstruction_controller` controller fn
    Create `deleteInstruction_controller` controller function
    in `src/controllers/instructions` directory, which is the
    function that receives the requests of deleting instructions
    from a dataset, calls instructions service to serve these
    requests and returns responses based of instructions
    service result.

---
+++ src/controllers/instructions/deleteInstruction_controller.ts
@@ -0,0 +1,27 @@
+import ErrorResponse from "../../utilities/ErrorResponse";
+import InternalServerErrorResponse from "../../utilities/InternalServerErrorResponse";
+import SuccessResponse from "../../utilities/SuccessResponse";
+import instructionsService from "../../services/instructions";
+import type { Req } from "../../types/request";
+
+export default async function deleteInstruction_controller(
+  request: Req
+): Promise<Response> {
+  try {
+    const { result, isSuccess, message } =
+      await instructionsService.deleteInstruction(
+        request.search.datasetId,
+        request.search.instructionId
+      );
+
+    if (isSuccess) {
+      return SuccessResponse(result, message);
+    }
+    return ErrorResponse(
+      message || "Failed to delete the instruction from the dataset",
+      400
+    );
+  } catch {
+    return InternalServerErrorResponse();
+  }
+}
