c82cd61d2206bfe5400c4157f00dcd8ed8bd42ee
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-05 15:41:51 +0300

    feat: create `getRecentActivities_controller` controller
    Create `getRecentActivities_controller` which is the route
    handler or the controller of reading the recent activities
    requests.

---
+++ src/controllers/activities/getRecentActivities_controller.ts
@@ -0,0 +1,25 @@
+import ErrorResponse from "../../utilities/ErrorResponse";
+import InternalServerErrorResponse from "../../utilities/InternalServerErrorResponse";
+import SuccessResponse from "../../utilities/SuccessResponse";
+import type { Req } from "../../types/request";
+import activitiesService from "../../services/activities";
+
+export default async function getRecentActivities_controller(
+  _request: Req
+): Promise<Response> {
+  try {
+    const { message, isSuccess, result } =
+      await activitiesService.getRecentActivities();
+
+    if (isSuccess) {
+      return SuccessResponse(result, message);
+    } else {
+      return ErrorResponse(
+        message || "Error while getting the recent activity",
+        400
+      );
+    }
+  } catch {
+    return InternalServerErrorResponse();
+  }
+}
