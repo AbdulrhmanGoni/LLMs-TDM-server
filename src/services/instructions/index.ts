aeff89fbc4a6bbb503aab029f3f9ef7c3a1e594b
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-06 13:17:06 +0300

    feat: add `getInstructionsByIds` method to `InstructionsService`
    Add `getInstructionsByIds_service` service function to the class
    `InstructionsService` as `getInstructionsByIds` method.

--- src/services/instructions/index.ts
+++ src/services/instructions/index.ts
@@ -1,5 +1,6 @@
 import addInstruction_service from "./addInstruction_service";
 import getInstructions_service from "./getInstructions_service";
+import getInstructionsByIds_service from "./getInstructionsByIds_service";
 import deleteInstruction_service from "./deleteInstruction_service";
 import updateInstruction_service from "./updateInstruction_service";
 
@@ -9,6 +10,8 @@ class InstructionsService {
   addInstruction = addInstruction_service;
 
   getInstructions = getInstructions_service;
+
+  getInstructionsByIds = getInstructionsByIds_service;
 
   updateInstruction = updateInstruction_service;
 
