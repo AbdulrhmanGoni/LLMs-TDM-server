9636d690fa73775fcd0fdc0763b30b23e2bbfc5c
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-23 20:36:51 +0300

    feat: create export datasets routes registerer
    Create the routes registerer of "export datasets" feature
    in `src/routes/exportRoutes.ts` file and make this register
    registers  "/export/:datasetId" endpoint to handle exporting
    datasets requests.
    
    Run "export datasets" registerer function inside `routesRegistering` function in `src/routes/index.ts` file to "exporting datasets" feature
    be included in the app.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import type { RouterType } from "../router/Router";
 import datasetsRoutesRegisterer from "./datasetsRoutes";
 import instructionsRoutesRegisterer from "./instructionsRoutes";
 import recentActivitiesRoutesRegisterer from "./recentActivitiesRoutes";
+import exportRoutesRegisterer from "./exportRoutes";
 
 export default function routesRegistering(router: RouterType) {
   datasetsRoutesRegisterer(router);
   instructionsRoutesRegisterer(router);
   recentActivitiesRoutesRegisterer(router);
+  exportRoutesRegisterer(router);
 }
