d74e2a71d900f7230ae3279cd5422fbfb423510c
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-24 23:11:45 +0300

    feat: add `authentication` to global middlewares array

--- src/middlewares/globalMiddlewares.ts
+++ src/middlewares/globalMiddlewares.ts
@@ -1,5 +1,6 @@
 import type { RequestHandler } from "../types/request";
+import authentication from "./authentication";
 
-const globalMiddlewares: RequestHandler[] = [];
+const globalMiddlewares: RequestHandler[] = [authentication];
 
 export default globalMiddlewares;
