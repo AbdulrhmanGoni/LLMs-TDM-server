5c3aa81b20140fa04cb3a80a88be911b5a205320
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-26 11:35:27 +0300

    feat: add `dataset` & `instruction` properties to `Activity` type

--- src/types/activities.ts
+++ src/types/activities.ts
@@ -1,5 +1,5 @@
 import type { Types } from "mongoose";
-import type { Dataset } from "./datasets";
+import type { DatasetBase } from "./datasets";
 import type { InstructionBase } from "./instructions";
 
 export type ActivityResource = "Instructions" | "Datasets";
@@ -7,19 +7,13 @@ export type ActivityResource = "Instructions" | "Datasets";
 export type ActivitiesTypes = "New Resource" | "Modification" | "Deletion";
 
 export type DatasetActivity = {
-  datasetId: Types.ObjectId;
-  activity: ActivityResource;
+  dataset: DatasetBase & { _id: Types.ObjectId };
+  activity: ActivitiesTypes;
   activityDate: Date;
-  deletedResource?: Pick<Dataset, "name" | "description"> | InstructionBase;
 };
 
 export type InstructionActivity = DatasetActivity & {
-  instructionId: Types.ObjectId;
+  instruction: InstructionBase & { _id: Types.ObjectId };
 };
 
 export type Activity = DatasetActivity | InstructionActivity;
-
-export type ActivityFilter = {
-  instructionId?: Types.ObjectId;
-  datasetId: Types.ObjectId;
-};
