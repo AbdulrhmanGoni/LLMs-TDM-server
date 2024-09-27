cf1b7d7a1894d1251a0c6bb463ce4fc6f1e99a69
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-27 18:46:55 +0300

    feat: add new deleting procces to `deleteDataset` method
    Use `deleteAllDatasetInstructions` method of `instructionsService`
    service object to delete the all instructions of the dataset that is
    being deleting if exist in `deleteDataset_service` service function.

--- src/services/datasets/deleteDataset_service.ts
+++ src/services/datasets/deleteDataset_service.ts
@@ -1,17 +1,21 @@
 import DatasetsModel from "../../models/DatasetsModel";
 import type { Dataset } from "../../types/datasets";
 import type { ServiceOperationResultType } from "../../types/response";
+import createTransactionSession from "../../utilities/createTransactionSession";
 import ServiceOperationResult from "../../utilities/ServiceOperationResult";
 import activitiesService from "../activities";
+import instructionsService from "../instructions";
 
 export default async function deleteDataset_service(
   userId: string,
   datasetId: Dataset["id"]
 ): Promise<ServiceOperationResultType> {
+  const session = await createTransactionSession();
+
   const result = await DatasetsModel.findByIdAndUpdate(
     { _id: userId },
     { $pull: { datasets: { _id: datasetId } } },
-    { projection: { datasets: 1 } }
+    { projection: { datasets: 1 }, session }
   );
 
   if (result) {
@@ -20,26 +24,39 @@ export default async function deleteDataset_service(
     );
 
     if (deletedDataset) {
-      activitiesService.registerDatasetActivity(
-        userId,
-        {
-          name: deletedDataset.name,
-          description: deletedDataset.description,
-          _id: deletedDataset._id,
-        },
-        new Date(),
-        "Deletion"
-      );
-      return ServiceOperationResult.success(
-        true,
-        `The dataset was deleted successfully`
-      );
+      const { isSuccess, message } =
+        await instructionsService.deleteAllDatasetInstructions(datasetId, {
+          session,
+        });
+
+      if (isSuccess) {
+        activitiesService.registerDatasetActivity(
+          userId,
+          {
+            name: deletedDataset.name,
+            description: deletedDataset.description,
+            _id: deletedDataset._id,
+          },
+          new Date(),
+          "Deletion"
+        );
+        await session.commitTransaction();
+        return ServiceOperationResult.success(
+          true,
+          `The dataset was deleted successfully`
+        );
+      } else {
+        session.abortTransaction();
+        return ServiceOperationResult.failure(message);
+      }
     } else {
+      session.abortTransaction();
       return ServiceOperationResult.failure(
         `There is no dataset with "${datasetId}" id`
       );
     }
   } else {
+    session.abortTransaction();
     return ServiceOperationResult.failure(
       `There is no user with "${userId}" id`
     );
