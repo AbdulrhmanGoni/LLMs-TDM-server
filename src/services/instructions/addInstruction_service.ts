6f6b7a9ffa0741cf5294ca3f6a4ddacde122f81d
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-27 14:23:35 +0300

    refactor: `addInstruction` service & controller functions
    Make `addInstruction_service` function receives user id
    as parameter from `addInstruction_controller` to be able to
    use `incrementInstructionsCount` method of `datasetsService`
    object to increment the instructions count.
    
    And also make this function use `InstructionModel` as a
    model directlly to add the instructions to instructions
    collection instead of calling it to get the model.
    
    Also make this function passes the user id and the data
    of the created instruction to `registerInstructionActivity`
    method to register the activity.

--- src/services/instructions/addInstruction_service.ts
+++ src/services/instructions/addInstruction_service.ts
@@ -1,4 +1,3 @@
-import { Types } from "mongoose";
 import createTransactionSession from "../../utilities/createTransactionSession";
 import ServiceOperationResult from "../../utilities/ServiceOperationResult";
 import InstructionModel from "../../models/InstructionModel";
@@ -8,47 +7,50 @@ import datasetsService from "../datasets";
 import activitiesService from "../activities";
 
 export default async function addInstruction_service(
+  userId: string,
   instructionInput: AddInstructionInput
 ): Promise<ServiceOperationResultType> {
   const session = await createTransactionSession();
 
-  const { Model, failure } = await InstructionModel(instructionInput.datasetId);
+  const newInstruction = new InstructionModel(instructionInput);
+  const addInstructionResult = await newInstruction
+    .save({ session })
+    .then(() => true)
+    .catch(() => false);
 
-  if (Model) {
-    const newInstruction = new Model(instructionInput);
-    const addInstructionResult = await newInstruction
-      .save({ session })
-      .then(() => true)
-      .catch(() => false);
+  if (addInstructionResult) {
+    const incrementInstructionsCountResult =
+      await datasetsService.incrementInstructionsCount(
+        userId,
+        instructionInput.datasetId,
+        1,
+        session
+      );
 
-    if (addInstructionResult) {
-      const incrementInstructionsCountResult =
-        await datasetsService.incrementInstructionsCount(
-          instructionInput.datasetId,
-          1,
-          session
-        );
+    if (incrementInstructionsCountResult.isSuccess) {
+      activitiesService.registerInstructionActivity(
+        userId,
+        instructionInput.datasetId,
+        {
+          _id: newInstruction._id,
+          systemMessage: newInstruction.systemMessage,
+          question: newInstruction.question,
+          answer: newInstruction.answer,
+        },
+        newInstruction.createdAt,
+        "New Resource"
+      );
 
-      if (incrementInstructionsCountResult.isSuccess) {
-        await session.commitTransaction();
-        activitiesService.registerInstructionActivity(
-          new Types.ObjectId(instructionInput.datasetId),
-          newInstruction._id,
-          newInstruction.createdAt,
-          "New Resource"
-        );
-        return ServiceOperationResult.success(
-          newInstruction,
-          `The instruction added to the dataset successfully`
-        );
-      }
+      await session.commitTransaction();
+      return ServiceOperationResult.success(
+        newInstruction,
+        `The instruction added to the dataset successfully`
+      );
     }
-
-    await session.abortTransaction();
-    return ServiceOperationResult.failure(
-      `Adding the instruction to the dataset failed`
-    );
-  } else {
-    return failure;
   }
+
+  await session.abortTransaction();
+  return ServiceOperationResult.failure(
+    `Adding the instruction to the dataset failed`
+  );
 }
