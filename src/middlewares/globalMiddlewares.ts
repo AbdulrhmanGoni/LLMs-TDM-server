d5342c1351141545c9650201b1ca66830f4e3211
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-28 15:39:40 +0300

    feat: create `websocketGlobalMiddlewares` array
    Create `websocketGlobalMiddlewares` middlewares array in
    `src/middlewares/globalMiddlewares.ts` file and export it,
    
    This array should contains the global middlewares that
    supposed to be run before any web socket route handler

--- src/middlewares/globalMiddlewares.ts
+++ src/middlewares/globalMiddlewares.ts
@@ -1,6 +1,11 @@
-import type { RequestHandler } from "../types/request";
+import type { RequestHandler, WebSocketHandlers } from "../types/request";
 import authentication from "./authentication";
+import webSocketAuthentication from "./webSocketAuthentication";
 
 const globalMiddlewares: RequestHandler[] = [authentication];
+
+export const websocketGlobalMiddlewares: WebSocketHandlers[] = [
+  webSocketAuthentication,
+];
 
 export default globalMiddlewares;
