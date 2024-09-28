e2624bfc291d650ea64236e65b65c604251f91ef
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-28 11:24:02 +0300

    feat: create dataset instructions stream service function
    Create `datasetInstructionsStream_service` function to provide
    a simple and direct way to read the instructions of a specific
    dataset in chunks to export the instructions of the dataset as
    file (in `csv` or `jsonl` format).
    
    The function uses `getDatasetInstructionsReader` function to
    get the cursor that allows it to read the instructions from the
    database group by group and it takes each group of instructions
    and formats them in the requested format and calls the callback
    function `onChunk` (that is passed as parapeter) with the formated
    group of instructions as chunk of file.

---
+++ src/services/export/datasetInstructionsStream_service.ts
@@ -0,0 +1,59 @@
+import type { Dataset } from "../../types/datasets";
+import type { DatasetFormat } from "../../types/datasets";
+import getDatasetInstructionsReader from "./getDatasetInstructionsReader";
+
+export type DatasetInstructionsStreamOptions = {
+  userId: string;
+  datasetId: Dataset["id"];
+  datasetFormat: DatasetFormat;
+  onChunk: (chunk: string, progress: number, done?: boolean) => void;
+};
+
+export default async function datasetInstructionsStream_service({
+  userId,
+  datasetId,
+  datasetFormat,
+  onChunk,
+}: DatasetInstructionsStreamOptions) {
+  const { cursor, dataset, failure } = await getDatasetInstructionsReader(
+    userId,
+    datasetId
+  );
+  if (cursor) {
+    const instructionsPerchunks = 10;
+    const totalChunks = Math.ceil(
+      dataset.instructionsCount / instructionsPerchunks
+    );
+    let progress = 0;
+    let done = false;
+
+    await cursor.eachAsync(
+      async (instruction, i) => {
+        progress = +(((i + 1) / totalChunks) * 100).toFixed(2);
+        done = i + 1 === totalChunks;
+        onChunk(
+          instruction.reduce((chunk, instruction) => {
+            chunk += datasetFormat.formater({
+              systemMessage: instruction.systemMessage || undefined,
+              question: instruction.question,
+              answer: instruction.answer,
+            });
+
+            if (done && datasetFormat.decorators?.last) {
+              return chunk + datasetFormat.decorators?.last || "";
+            }
+
+            return chunk;
+          }, datasetFormat.decorators?.first || ""),
+          progress,
+          done
+        );
+      },
+      { batchSize: instructionsPerchunks }
+    );
+
+    return true;
+  } else {
+    return failure;
+  }
+}
