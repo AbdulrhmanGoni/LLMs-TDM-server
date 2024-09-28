bc09ea9f5b248159f3dace0bdacc37fe434bf61c
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-28 17:12:04 +0300

    feat: create `exportDatasetWS_controller` function
    Create `exportDatasetWS_controller` function which is acts
    as presentation layer of "export datasets through web sockets"
    service, Receives the web sockets connections and call
    `exportDatasetWS` to start exporting the dataset.
    
    Add `exportDatasetWS_controller` function as `exportDatasetWS`
    method to `ExportDatasetController` class.

--- src/controllers/export/index.ts
+++ src/controllers/export/index.ts
@@ -1,9 +1,11 @@
 import exportDataset_controller from "./exportDataset_controller";
+import exportDatasetWS_controller from "./exportDatasetWS_controller";
 
 class ExportDatasetController {
   constructor() {}
 
   exportDataset = exportDataset_controller;
+  exportDatasetWS = exportDatasetWS_controller;
 }
 
 const exportDatasetController = new ExportDatasetController();
