e55511c0cb1d3a76e4ee359a19ccdaf1cc064913
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-07 10:54:09 +0300

    refactor: results messages in `getDatasets_service.ts`
    Reuse `operationsResultsMessages` in `getDatasets_service.ts`
    file to return results messages.
    
    Add check condition to `getDatasets_service` function if
    user's datasets document exists or not and return "no user"
    message if not exists.

--- src/services/datasets/getDatasets_service.ts
+++ src/services/datasets/getDatasets_service.ts
@@ -1,14 +1,21 @@
 import ServiceOperationResult from "../../utilities/ServiceOperationResult";
 import DatasetsModel from "../../models/DatasetsModel";
 import type { ServiceOperationResultType } from "../../types/response";
+import operationsResultsMessages from "../../constants/operationsResultsMessages";
 
 export default async function getDatasets_service(
   userId: string
 ): Promise<ServiceOperationResultType> {
   const result = await DatasetsModel.findOne({ _id: userId });
 
-  return ServiceOperationResult.success(
-    result?.datasets || [],
-    result?.datasets.length ? undefined : "There are no datasets yet"
-  );
+  if (result) {
+    return ServiceOperationResult.success(
+      result.datasets,
+      result.datasets.length ? undefined : operationsResultsMessages.noDatasets
+    );
+  } else {
+    return ServiceOperationResult.failure(
+      operationsResultsMessages.noUser(userId)
+    );
+  }
 }
