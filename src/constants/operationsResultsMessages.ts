770817e7558f1f4c9eb7637489cdb8b05b67f3c0
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-07 10:38:04 +0300

    refactor: collect all operations results messages in one file
    Create `src/constants` directory and create inside this directory
    `operationsResultsMessages.ts` file to collect all operations results
    messages in it for reusability and easier maintainability.

---
+++ src/constants/operationsResultsMessages.ts
@@ -0,0 +1,30 @@
+const operationsResultsMessages = {
+  successfulDatasetCreation: (datasetName: string) =>
+    `"${datasetName}" dataset created successfuly`,
+  successfulDatasetUpdate: "The dataset was updated successfully",
+  failedDatasetCreation: (datasetName: string) =>
+    `"${datasetName}" dataset creation failed`,
+  noDatasets: "There are no datasets yet",
+  noDataset: (datasetId: string) =>
+    `There is no dataset with "${datasetId}" id`,
+  successfulDatasetDeletion: "The dataset was deleted successfully",
+  failedDatasetDeletion: "",
+  noInstructionsForDataset: (datasetId: string) =>
+    `No instructions found for a dataset with this id: ${datasetId}`,
+
+  successfulInstructionAddition:
+    "The instruction was added to the dataset successfully",
+  failedInstructionAddition: "Adding the instruction to the dataset failed",
+  successfulInstructionDeletion:
+    "The instruction was deleted from the dataset successfully",
+  failedInstructionDeletion:
+    "Failed to delete the instruction from the dataset",
+  noInstructionToDelete:
+    "The targeted instruction not found to delete it from the dataset",
+  successfullInstructionUpdate: "The instruction updated successfully",
+  failedInstructionUpdate: "Failed to update the instruction",
+
+  noUser: (userId: string) => `There is no user with "${userId}" id`,
+};
+
+export default operationsResultsMessages;
