deadaae87485250517ed78f37d314cbb53be9552
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-13 23:08:13 +0300

    feat: use logging service when database connection failed
    Use `loggerService` service object to log an error when database
    connection failed in `src/configurations/databaseConnection.ts`
    file.

--- src/configurations/databaseConnection.ts
+++ src/configurations/databaseConnection.ts
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
+import loggerService from "../services/logger";
 
 export default async function databaseConnection() {
   try {
     await mongoose.connect(import.meta.env.DB_URL);
-    console.log("The server connected to the database ✅");
+    console.log("The server connected to the database successfully ✅");
   } catch (error: any) {
-    throw new Error(
-      "The server failed to connect to the database ❌: " + error?.message
-    );
+    const message =
+      "The server failed to connect to the database ❌: " + error?.message;
+
+    loggerService.error(message, {
+      operation: "Database Connection",
+    });
+
+    throw new Error(message);
   }
 }
