2fc2c4e4f9530784cb2dcf861d03fc03d193d58a
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-15 14:42:46 +0300

    chore: update `scripts/startDevelopmentServer.ts` script
    Update the usage of `initializeMongodbReplSet` function which
    was refactored and became receives an additional parameter in
    `scripts/startDevelopmentServer.ts` script file.
    
    Also make this script file starts the server as docker container
    by running `docker:dev` command instead of `bun start:dev`
    command which starts the server on the local machine.

--- scripts/startDevelopmentServer.ts
+++ scripts/startDevelopmentServer.ts
@@ -1,8 +1,8 @@
 import { $ } from "bun";
-import initializeMongodbReplSet from "./initializeMongodbReplSet.js";
+import initializeReplSetOf from "./initializeReplSetOf.js";
 
 await $`docker-compose -f docker-compose-dev-mongodb.yaml up -d`;
 
-await initializeMongodbReplSet("primary-dev-db");
+await initializeReplSetOf("primary-dev-db", "devAndTestReplSetConfig");
 
-await $`bun start:dev`;
+await $`bun docker:dev`;
