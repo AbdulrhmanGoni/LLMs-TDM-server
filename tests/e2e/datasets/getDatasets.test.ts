9dcb31351cbca62219c65eb28adc7edff8e6f22f
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-19 15:29:43 +0300

    test: write datasets retrieving e2e tests
    Create `datasets/getDatasets.test.ts` file of dataset retrieving
    e2e tests and write inside it two retrieving test cases.

---
+++ tests/e2e/datasets/getDatasets.test.ts
@@ -0,0 +1,54 @@
+import { describe, expect, it, afterAll } from "bun:test";
+import { request } from "../..";
+import operationsResultsMessages from "../../../src/constants/operationsResultsMessages";
+import DatasetsModel from "../../../src/models/DatasetsModel";
+import { fakeDatasets } from "../../fake-data/fakeDatasets";
+import { afterEach } from "bun:test";
+import RecentActivitiesModel from "../../../src/models/RecentActivitiesModel";
+
+const path = "datasets";
+
+describe(`GET /${path}`, () => {
+  it("Should return an ampty datasets array", async () => {
+    await DatasetsModel.create({
+      datasets: [],
+      _id: process.env.TESTING_USER_ID,
+    });
+
+    const { resBody, status } = await request.GET(path);
+
+    expect(status).toBe(200);
+    expect(resBody.data).toBeInstanceOf(Array);
+    expect(resBody.data).toBeEmpty();
+    expect(resBody.message).toBe(operationsResultsMessages.noDatasets);
+  });
+
+  it("Should return an array of all datasets", async () => {
+    await DatasetsModel.create({
+      datasets: fakeDatasets,
+      _id: process.env.TESTING_USER_ID,
+    });
+
+    const { resBody, status } = await request.GET(path);
+
+    expect(status).toBe(200);
+    expect(resBody.data).toBeInstanceOf(Array);
+    expect(resBody.data.length).toBe(fakeDatasets.length);
+    expect(resBody.data).toContainEqual({
+      _id: expect.toBeObjectId(),
+      name: expect.any(String),
+      description: expect.any(String),
+      instructionsCount: 0,
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+  });
+});
+
+afterEach(async () => {
+  await DatasetsModel.deleteMany();
+});
+
+afterAll(async () => {
+  await RecentActivitiesModel.deleteMany();
+});
