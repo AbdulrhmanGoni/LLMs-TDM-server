1b749d4bfebd8cb6d264494fb55889fabc30734d
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-19 15:09:18 +0300

    test: write dataset deletion e2e tests
    Create `datasets/deleteDataset.test.ts` file of dataset
    deletion e2e tests and write inside it two deletion test
    cases.

---
+++ tests/e2e/datasets/deleteDataset.test.ts
@@ -0,0 +1,55 @@
+import { afterAll, describe, expect, it } from "bun:test";
+import { request } from "../..";
+import { getRandomFakeDataset } from "../../fake-data/fakeDatasets";
+import DatasetsModel from "../../../src/models/DatasetsModel";
+import RecentActivitiesModel from "../../../src/models/RecentActivitiesModel";
+import operationsResultsMessages from "../../../src/constants/operationsResultsMessages";
+import { afterEach } from "bun:test";
+
+const path = "datasets";
+
+describe(`DELETE /${path}`, () => {
+  it("Should response an error because the dataset is not existent", async () => {
+    const fakeDataset = getRandomFakeDataset();
+
+    await DatasetsModel.create({
+      datasets: [],
+      _id: process.env.TESTING_USER_ID,
+    });
+
+    const { resBody, status } = await request.DELETE(
+      `${path}/${fakeDataset._id.toHexString()}`
+    );
+
+    expect(status).toBe(400);
+    expect(resBody.message).toBe(
+      operationsResultsMessages.noDataset(fakeDataset._id.toHexString())
+    );
+  });
+
+  it("Should delete the dataset successflly", async () => {
+    const fakeDataset = getRandomFakeDataset();
+
+    await DatasetsModel.create({
+      datasets: [fakeDataset],
+      _id: process.env.TESTING_USER_ID,
+    });
+
+    const { resBody, status } = await request.DELETE(
+      `${path}/${fakeDataset._id.toHexString()}`
+    );
+
+    expect(status).toBe(200);
+    expect(resBody.message).toBe(
+      operationsResultsMessages.successfulDatasetDeletion
+    );
+  });
+});
+
+afterEach(async () => {
+  await DatasetsModel.deleteMany();
+});
+
+afterAll(async () => {
+  await RecentActivitiesModel.deleteMany();
+});
