9cc902c2b294ba626669ecf7c7b339db954857fb
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-20 15:10:32 +0300

    chore: replace `docker-compose` usage with `docker compose`

--- scripts/setupTestsEnvironment.ts
+++ scripts/setupTestsEnvironment.ts
@@ -2,6 +2,6 @@ import { $ } from "bun";
 import initializeMongodbReplSet from "./initializeReplSetOf";
 import "../tests/lib/customMatchers.ts";
 
-await $`docker-compose -f docker-compose-test-mongodb.yaml up -d`;
+await $`docker compose -f docker-compose-test-mongodb.yaml up -d`;
 
 await initializeMongodbReplSet("primary-test-db", "testingReplSetConfig");
--- scripts/startDevelopmentServer.ts
+++ scripts/startDevelopmentServer.ts
@@ -1,7 +1,7 @@
 import { $ } from "bun";
 import initializeReplSetOf from "./initializeReplSetOf";
 
-await $`docker-compose -f docker-compose-dev-mongodb.yaml up -d`;
+await $`docker compose -f docker-compose-dev-mongodb.yaml up -d`;
 
 await initializeReplSetOf("primary-dev-db", "developmentReplSetConfig");
 
