b0cf22fa0f1f45eec446cad219289a3457066c89
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-23 11:39:28 +0300

    feat: add first 2 methods to `HuggingfaceService` class
    Add `createHuggingfaceAccount` and `huggingfaceOAuthCallback`
    methods to `HuggingfaceService` class.

--- src/services/huggingface/index.ts
+++ src/services/huggingface/index.ts
@@ -1,7 +1,12 @@
+import createHuggingfaceAccount_service from "./createHuggingfaceAccount_service";
+import huggingfaceOAuthCallback_service from "./huggingfaceOAuthCallback_service";
 
 export class HuggingfaceService {
   constructor() {}
 
+  createHuggingfaceAccount = createHuggingfaceAccount_service;
+
+  huggingfaceOAuthCallback = huggingfaceOAuthCallback_service;
 }
 
 const huggingfaceService = new HuggingfaceService();
