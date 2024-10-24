6a8d2fe0e99a90650fa7fec61838e47d1678112b
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-24 21:03:44 +0300

    feat: create huggingFace OAuth token endpoint requests
    Create `huggingFaceOAuthTokenRequests.ts` file and create
    inside it:
    - `huggingFaceOAuthTokenEndpointRequest` function which
    is the function that takes request's payload and sends the
    request to huggingFace OAuth token endpoint.
    
    - `getAccessToken` function to handle sending getting users
    access tokens requests.
    
    - `refreshAccessToken` function to handle sending refreshing
    access tokens requests.

---
+++ src/services/huggingface/huggingFaceOAuthTokenRequests.ts
@@ -0,0 +1,42 @@
+export async function huggingFaceOAuthTokenEndpointRequest<T extends object>(
+  requestPayload: T
+) {
+  const payload = {
+    client_id: process.env.HF_CLIENT_ID,
+    client_secret: process.env.HF_CLIENT_SECRET,
+    ...requestPayload,
+  };
+
+  let encodedFormBody = Object.entries(payload)
+    .map(
+      ([key, value]) =>
+        encodeURIComponent(key) + "=" + encodeURIComponent(value)
+    )
+    .join("&");
+
+  const response = await fetch("https://huggingface.co/oauth/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encodedFormBody,
+  });
+
+  return {
+    success: response.ok && response.status,
+    payload: await response.json(),
+  };
+}
+
+export async function getAccessTokens(code: string) {
+  return await huggingFaceOAuthTokenEndpointRequest({
+    code,
+    grant_type: "authorization_code",
+    redirect_uri: process.env.HF_AUTH_REDIRECT_URL,
+  });
+}
+
+export async function refreshAccessToken(refreshToken: string) {
+  return await huggingFaceOAuthTokenEndpointRequest({
+    grant_type: "refresh_token",
+    refresh_token: refreshToken,
+  });
+}
