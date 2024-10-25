16f659f013988ee3ee3627af81d2b0f4dc01e53b
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-25 14:19:16 +0300

    feat: create `huggingfaceOAuthCallback_controller`
    Create `huggingfaceOAuthCallback_controller` controller
    function and add it to `HuggingfaceController` class as
    `huggingfaceOAuthCallback` method to handle the requests
    of users `huggingface.co` accounts authorization.

--- src/controllers/huggingface/index.ts
+++ src/controllers/huggingface/index.ts
@@ -1,6 +1,9 @@
+import huggingfaceOAuthCallback_controller from "./huggingfaceOAuthCallback_controller";
 
 class HuggingfaceController {
   constructor() {}
+
+  huggingfaceOAuthCallback = huggingfaceOAuthCallback_controller;
 
 }
 
