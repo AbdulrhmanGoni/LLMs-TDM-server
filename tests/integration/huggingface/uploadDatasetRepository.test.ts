47eb516b691e39bdbef8450c5d4c0406a48b7f59
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-11-06 11:13:23 +0300

    test: write tests for `uploadDatasetRepository` method
    Create `uploadDatasetRepository.test.ts` file and write in
    it two integration tests for `uploadDatasetRepository` service
    method of `HuggingfaceService` class.

---
+++ tests/integration/huggingface/uploadDatasetRepository.test.ts
@@ -0,0 +1,88 @@
+import { describe, expect, it, afterAll, beforeAll, mock } from "bun:test";
+import operationsResultsMessages from "../../../src/constants/operationsResultsMessages";
+import DatasetsModel from "../../../src/models/DatasetsModel";
+import databaseConnection from "../../../src/configurations/databaseConnection";
+import huggingfaceService from "../../../src/services/huggingface";
+import { fakeUserHuggingfaceAccount } from "../../fake-data/fakeUserHuggingfaceAccount";
+import { getRandomFakeDataset } from "../../fake-data/fakeDatasets";
+
+const uploadFileMock = mock(async () => ({}))
+mock.module("@huggingface/hub", () => {
+  return { uploadFile: uploadFileMock };
+});
+
+let fakeDataset = getRandomFakeDataset()
+
+beforeAll(async () => {
+  await databaseConnection();
+  await DatasetsModel.create({
+    _id: process.env.TESTING_USER_ID,
+    datasets: [fakeDataset],
+    huggingfaceAccount: fakeUserHuggingfaceAccount
+  })
+});
+
+describe("Test `uploadDatasetRepository` service method", () => {
+  it("Should fail to upload the dataset because the dataset has not linked repository", async () => {
+    const result = await huggingfaceService.uploadDatasetRepository({
+      userId: process.env.TESTING_USER_ID,
+      datasetId: fakeDataset._id.toString(),
+    });
+
+    expect(result.isSuccess).toBeFalse();
+    expect(uploadFileMock).not.toHaveBeenCalled();
+    expect(result.message).toBe(operationsResultsMessages.noLinkedDatasetRepository);
+  });
+
+  it("Should complete uploading dataset repository process successully", async () => {
+
+    const datasetRepository = {
+      fileFormat: "CSV",
+      filePath: "data/filePath",
+      name: "username/datasetname",
+    }
+
+    const commit = {
+      commitTitle: "commit title",
+      commitDescription: "commit description",
+    }
+
+    await DatasetsModel.updateOne(
+      { _id: process.env.TESTING_USER_ID },
+      {
+        datasets: [
+          {
+            ...fakeDataset,
+            repository: datasetRepository
+          }
+        ],
+      }
+    )
+
+    const result = await huggingfaceService.uploadDatasetRepository({
+      userId: process.env.TESTING_USER_ID,
+      datasetId: fakeDataset._id.toString(),
+      ...commit
+    });
+
+    expect(result.isSuccess).toBeTrue();
+    expect(uploadFileMock).toHaveBeenCalledWith({
+      ...commit,
+      file: {
+        path: datasetRepository.filePath,
+        content: expect.any(Blob),
+      },
+      repo: {
+        name: datasetRepository.name,
+        type: "dataset",
+      },
+      accessToken: fakeUserHuggingfaceAccount.accessToken,
+    });
+    expect(result.message).toBe(operationsResultsMessages.successullyDatasetUpload);
+  });
+});
+
+afterAll(async () => {
+  mock.restore()
+  await DatasetsModel.deleteMany();
+});
